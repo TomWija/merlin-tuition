c4d1161726f11e360e21396d91debc538d2debbe
Thomas Wija <thomas.wija@gmail.com>
2017-04-06 08:19:25 +0100

    Heading and Branding styling

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -1,4 +1,6 @@
 <div class="container-fluid heading">
-	<h1 class='heading__branding--primary' style="margin: 0px;">Merlin Tuition</h1>
-	<h5 class='heading__branding--secondary'>Bespoke Tuition in Brighton and Hove</h5>
+	<div class="container">
+		<h1 class='heading__branding--primary' style="margin: 0px;">Merlin Tuition</h1>
+		<h5 class='heading__branding--secondary'>Bespoke Tuition in Brighton and Hove</h5>
+	</div>
 </div>
