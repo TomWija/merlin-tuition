f744ab8a75e7663d4f9afca2308e4eb3641c19bf
Thomas Wija <thomas.wija@gmail.com>
2017-04-07 08:38:01 +0100

    Started mobile navigation

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -1,6 +1,15 @@
 <div class="container-fluid heading">
 	<div class="container">
-		<h1 class='heading__branding--primary' style="margin: 0px;">Merlin Tuition</h1>
-		<h5 class='heading__branding--secondary'>Bespoke Tuition in Brighton and Hove</h5>
+		<h1 class="heading__branding--primary" style="margin: 0px;">Merlin Tuition</h1>
+		<h5 class="heading__branding--secondary">Bespoke Tuition in Brighton and Hove</h5>
+	</div>
+	<div class="heading__navigation">
+		<ul>
+			<li>Home</li>
+			<li>About Us</li>
+			<li>Become a Student</li>
+			<li>Become a Tutor</li>
+			<li>Contact Us</li>
+		</ul>
 	</div>
 </div>
