9453ac4f13577450e2ece3af3046ef019dbd4d13
Thomas Wija <thomas.wija@gmail.com>
2017-04-07 13:53:12 +0100

    Setting up hamburger icon

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -2,6 +2,7 @@
 	<div class="container">
 		<h1 class="heading__branding--primary" style="margin: 0px;">Merlin Tuition</h1>
 		<h5 class="heading__branding--secondary">Bespoke Tuition in Brighton and Hove</h5>
+		<div class="heading__hamburger"></div>
 	</div>
 	<div class="heading__navigation">
 		<ul>
