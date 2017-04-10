8d0e33d98bc624edaf5b24ed829e1235424305d4
Thomas Wija <thomas.wija@gmail.com>
2017-04-10 18:42:42 +0100

    Setting up home page CTA

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,17 @@
 @section('content')
 
 <div class="container-fluid splash">
-	boo
+	<form>
+		<select name="subject-one">
+			<option>English</option>
+		</select>
+
+		<select name="level-one">
+			<option>GCSE</option>
+		</select>
+
+		<input type="submit" value="Become a Student" />
+	</form>
 </div>
 
 <div style="background-color: #353C3F; width: 100vw; height: 200px;">
