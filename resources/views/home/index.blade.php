381cf62515c75e0f9b9b135942c56e2199d261f5
Thomas Wija <thomas.wija@gmail.com>
2017-04-11 00:01:27 +0100

    Working on main form cta

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,17 +3,21 @@
 @section('content')
 
 <div class="container-fluid splash">
-	<form>
-		<select name="subject-one">
-			<option>English</option>
-		</select>
-
-		<select name="level-one">
-			<option>GCSE</option>
-		</select>
-
-		<input type="submit" value="Become a Student" />
-	</form>
+	<div class="container">
+		<form>
+			<div class="rounded select--primary">
+				<select name="subject-one">
+					<option>English</option>
+				</select>
+			</div>
+			<div class="rounded select--primary">
+				<select name="level-one">
+					<option>GCSE</option>
+				</select>
+			</div>
+			<input class="cta--primary rounded" type="submit" value="Become a Student" />
+		</form>
+	</div>
 </div>
 
 <div style="background-color: #353C3F; width: 100vw; height: 200px;">
