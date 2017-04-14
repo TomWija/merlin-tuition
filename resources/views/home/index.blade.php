be93df36eb0f29ed870a5eab874f469d75550f8a
Thomas Wija <thomas.wija@gmail.com>
2017-04-14 12:57:52 +0100

    Finished main home template

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,21 +6,44 @@
 
 <div class="home container vertical-gutters--large">
 	<div class="row">
-		This is going to contain all the content for the home page.
-	</div>
-	<div class="row">
 		<div class="col-md-4">
-			Column 1
+			<div class="home__card">
+				<div class="home__card-image" style="background: url({{ asset('placeholder.png') }});"></div>
+				<h2>Column 1 Heading</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+				Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+				eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+			</div>
 		</div>
 
 		<div class="col-md-4">
-			Column 2
+			<div class="home__card">
+				<div class="home__card-image" style="background: url({{ asset('placeholder.png') }});"></div>
+				<h2>Column 2 Heading</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+				Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+				eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+			</div>
 		</div>
 
 		<div class="col-md-4">
-			Column 3
+			<div class="home__card">
+				<div class="home__card-image" style="background: url({{ asset('placeholder.png') }});"></div>
+				<h2>Column 3 Heading</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+				Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+				natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+				eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+			</div>
 		</div>
 	</div>
 </div>
+
+@include('components.testimonials')
 
 @endsection
