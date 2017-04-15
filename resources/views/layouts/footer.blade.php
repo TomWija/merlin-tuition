28477f48464263f6b653f8f81300e055c35ed0f3
Thomas Wija <thomas.wija@gmail.com>
2017-04-15 13:27:56 +0100

    Added a footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,2 +1,11 @@
-<div style="background-color: #353C3F; width: 100vw; height: 200px;">
+<div class="footer container-fluid vertical-gutters--medium">
+	<div class="row footer__copyright">
+		<div class="col-md-6">
+		Merlin Tuition is © {{ date('Y') }} of Merlin Stevenson
+		</div>
+
+		<div class="col-md-6 text-right">
+		Website designed and developed by Thomas Wija
+		</div>
+	</div>
 </div>
