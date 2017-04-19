3ad8e50fd968193e618fda148965ec271b0b4072
Thomas Wija <thomas.wija@gmail.com>
2017-04-19 08:36:54 +0100

    Added tables needed for dynamic/editable page content

---
+++ database/migrations/2017_04_19_083000_create_testimonials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestimonialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('student_name');
+            $table->string('subject');
+            $table->string('level');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+}
